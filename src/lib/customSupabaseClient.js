315550182ab9cf2bc6fb6f2c2b03aa758ac17da4
Kosan888 <bisniskosanku@gmail.com>
2026-02-12 18:52:17 +0700

    Create customSupabaseClient.js

---
+++ src/lib/customSupabaseClient.js
@@ -0,0 +1,14 @@
+import { createClient } from '@supabase/supabase-js';
+
+// Mengambil data dari brankas rahasia Netlify (Environment Variables)
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+const customSupabaseClient = createClient(supabaseUrl, supabaseAnonKey);
+
+export default customSupabaseClient;
+
+export { 
+    customSupabaseClient,
+    customSupabaseClient as supabase,
+};
